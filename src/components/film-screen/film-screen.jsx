2a497e130b65f0f400e3fbcf960260ac76d59ab7
Irina <raccoonlemon@gmail.com>
2020-10-08 23:39:46 +0600

    Переход на главную страницу по лого в подвале

--- src/components/film-screen/film-screen.jsx
+++ src/components/film-screen/film-screen.jsx
@@ -191,11 +191,11 @@ const FilmScreen = (props) => {
 
       <footer className="page-footer">
         <div className="logo">
-          <a href="main.html" className="logo__link logo__link--light">
+          <Link className="logo__link logo__link--light" to="/">
             <span className="logo__letter logo__letter--1">W</span>
             <span className="logo__letter logo__letter--2">T</span>
             <span className="logo__letter logo__letter--3">W</span>
-          </a>
+          </Link>
         </div>
 
         <div className="copyright">
