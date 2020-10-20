a2eb2995539b93dcb74b9c247a52fc9600a95a3b
Irina <raccoonlemon@gmail.com>
2020-10-20 22:01:22 +0600

    Добавлены вспомогательные функции

--- src/utils/common.js
+++ src/utils/common.js
@@ -6,3 +6,11 @@ export const toCamelCase = (s) => {
       .replace(`_`, ``);
   });
 };
+
+export const extend = (a, b) => {
+  return Object.assign({}, a, b);
+};
+
+export const capitalizeFirstLetter = (string) =>{
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
