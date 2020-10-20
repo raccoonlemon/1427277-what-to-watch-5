dc901ce417d3ce5550b529ad2a04bff39ba9e4ca
Irina <raccoonlemon@gmail.com>
2020-10-20 22:00:59 +0600

    Инициализировано хранилище

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
 import App from "./components/app/app";
+import {ALL_GENRES_FILTER} from "./const";
 import {films, promoFilm} from "./mocks/films.js";
 import {generateMockReviews} from "./mocks/reviews";
+import {reducer} from "./store/reducer";
+
+const preloadedState = {
+  genreFilter: ALL_GENRES_FILTER,
+  films
+};
+
+const store = createStore(reducer, preloadedState, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f);
 
 const reviews = generateMockReviews(films);
 
 ReactDOM.render(
-    <App
-      promoFilm = {promoFilm}
-      films = {films}
-      reviews = {reviews}/>,
+    <Provider store={store}>
+      <App
+        promoFilm = {promoFilm}
+        films = {films}
+        reviews = {reviews}/>
+    </Provider>,
     document.querySelector(`#root`)
 );
