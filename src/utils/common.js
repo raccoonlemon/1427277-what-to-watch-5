23211e73abe30f042f118afa6a19059c2b2c5505
Irina <raccoonlemon@gmail.com>
2020-10-22 19:11:32 +0600

    Правки после слияния веток

--- src/utils/common.js
+++ src/utils/common.js
@@ -7,7 +7,6 @@ export const toCamelCase = (s) => {
   });
 };
 
-<<<<<<< HEAD
 export const splitArrayToSegments = (array, segmentsCount)=> {
   let segmentLength = Math.ceil(array.length / segmentsCount);
 
@@ -20,12 +19,12 @@ export const splitArrayToSegments = (array, segmentsCount)=> {
   }
 
   return segments;
-=======
-export const extend = (a, b) => {
-  return Object.assign({}, a, b);
 };
 
 export const capitalizeFirstLetter = (string) =>{
   return string.charAt(0).toUpperCase() + string.slice(1);
->>>>>>> module5-task1
+};
+
+export const extend = (a, b) => {
+  return Object.assign({}, a, b);
 };
