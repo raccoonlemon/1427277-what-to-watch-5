9cd9a3e39a868976615165d06df12f2f794da694
Irina <raccoonlemon@gmail.com>
2020-10-27 20:53:27 +0600

    Добавлен HOC WithActiveItem в компонент FilmsList

--- src/components/films-list/films-list.jsx
+++ src/components/films-list/films-list.jsx
@@ -1,39 +1,32 @@
 import PropTypes from 'prop-types';
-import React, {PureComponent} from "react";
+import React from "react";
+import withActiveItem from '../../HOC/with-active-item';
 import {filmShape} from "../../utils/props-validation";
 import FilmSmallCard from "../film-small-card/film-small-card";
 
-class FilmsList extends PureComponent {
-  constructor(props) {
-    super(props);
+const FilmsList = (props) => {
+  const {films, setActiveItem, removeActiveItem, activeItemId} = props;
 
-    this.state = {
-      activeItemId: null
-    };
-  }
-
-  render() {
-    const films = this.props.films;
-    const filmsList = films.map((it)=>(
-      <FilmSmallCard
-        key = {it.id}
-        onMouseEnter = {(id)=>{
-          this.setState({activeItemId: id});
-        }}
-        onMouseLeave = {()=>{
-          this.setState({activeItemId: null});
-        }}
-        film = {it}
-        isCardActive = {it.id === this.state.activeItemId}/>));
-    return (
-      <div className="catalog__movies-list">
-        {filmsList}
-      </div>);
-  }
-}
+  const filmsList = films.map((it)=>(
+    <FilmSmallCard
+      key = {it.id}
+      onMouseEnter ={setActiveItem}
+      onMouseLeave = {removeActiveItem}
+      film = {it}
+      isCardActive = {it.id === activeItemId}/>));
+  return (
+    <div className="catalog__movies-list">
+      {filmsList}
+    </div>);
+};
 
 FilmsList.propTypes = {
+  setActiveItem: PropTypes.func.isRequired,
+  removeActiveItem: PropTypes.func.isRequired,
+  activeItemId: PropTypes.string.isRequired,
   films: PropTypes.arrayOf(filmShape).isRequired,
 };
 
-export default FilmsList;
+export {FilmsList};
+export default withActiveItem(FilmsList);
+
