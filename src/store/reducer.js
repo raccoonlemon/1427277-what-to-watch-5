c00ba68122e5f4e6f4c94ae15de7bb6bb51ed53b
Irina <raccoonlemon@gmail.com>
2020-10-31 16:48:37 +0600

    Удален неиспользуемый импорт

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,4 +1,4 @@
-import {ALL_GENRES_FILTER, SHOWN_FILMS_INITIAL_COUNT, SHOW_MORE_FILMS_STEP} from "../const";
+import {ALL_GENRES_FILTER, SHOWN_FILMS_INITIAL_COUNT} from "../const";
 import {extend} from "../utils/common";
 import {ActionType} from "./action";
 
