f4029b47d0d20ba1b2b3fd799dab0af87b668a4b
Irina <raccoonlemon@gmail.com>
2020-10-31 16:11:15 +0600

    Подключит redux-thunk к хранилищу

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
+import thunk from "redux-thunk";
 import App from "./components/app/app";
 import {films, promoFilm} from "./mocks/films.js";
 import {generateMockReviews} from "./mocks/reviews";
+import {createAPI} from "./services/api";
 import rootReducer from "./store/reducers/root-reducer";
 
-const reviews = generateMockReviews(films);
+const api = createAPI(()=>{});
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f);
+const reviews = generateMockReviews(films);
+// window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument(api)));
 
 ReactDOM.render(
     <Provider store={store}>
