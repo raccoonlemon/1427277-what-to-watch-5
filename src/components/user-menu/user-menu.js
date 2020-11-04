ab425160ef63fca817e50061de35cc193548770c
Irina <raccoonlemon@gmail.com>
2020-11-04 19:55:48 +0600

    Компонент UserMenu изменен с классового на функциональный

--- src/components/user-menu/user-menu.js
+++ src/components/user-menu/user-menu.js
@@ -1,36 +1,36 @@
 import PropTypes from 'prop-types';
-import React, {PureComponent} from "react";
+import React from "react";
 import {connect} from "react-redux";
 import {Link} from 'react-router-dom';
+import {Path} from '../../const';
 import {selectIsUserLogged} from "../../store/reducers/selectors";
 
+// TODO: найти откуда должна открываться страница Path.MY_LIST.
+// Не нашла в разметке. Временно добавила ссылку в аватаре пользователя.
 const renderUserBlock = (isLogged)=>{
   if (isLogged) {
     return (
       <div className="user-block__avatar">
-        <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+        <Link to = {Path.MY_LIST}>
+          <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+        </Link>
       </div>
     );
   }
 
-  return (<Link className="user-block__link" to = "/login">Sign in</Link>);
+  return (<Link className="user-block__link" to = {Path.SIGN_IN}>Sign in</Link>);
 };
 
-export class UserMenu extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
+const UserMenu = (props)=>{
+  const {isUserLogged} = props;
 
-  render() {
-    const {isUserLogged} = this.props;
-    return (
+  return (
+    <div className="user-block">
       <div className="user-block">
-        <div className="user-block">
-          {renderUserBlock(isUserLogged)}
-        </div>
-      </div>);
-  }
-}
+        {renderUserBlock(isUserLogged)}
+      </div>
+    </div>);
+};
 
 const mapStateToProps = (state) => ({
   isUserLogged: selectIsUserLogged(state)
