cd8f836e8d67555d251247b3fcc9b03991bd2676
Irina <raccoonlemon@gmail.com>
2020-11-10 20:14:17 +0600

    Рефакторинг <UserMenu>

--- src/components/user-menu/user-menu.js
+++ src/components/user-menu/user-menu.js
@@ -5,27 +5,19 @@ import {Link} from 'react-router-dom';
 import {Path} from '../../const';
 import {selectIsUserLogged} from "../../store/selectors";
 
-const renderUserBlock = (isLogged)=>{
-  if (isLogged) {
-    return (
-      <div className="user-block__avatar">
-        <Link to = {Path.MY_LIST}>
-          <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-        </Link>
-      </div>
-    );
-  }
-
-  return (<Link className="user-block__link" to = {Path.SIGN_IN}>Sign in</Link>);
-};
-
 const UserMenu = (props)=>{
   const {isUserLogged} = props;
 
   return (
     <div className="user-block">
       <div className="user-block">
-        {renderUserBlock(isUserLogged)}
+        {!isUserLogged && <Link className="user-block__link" to = {Path.SIGN_IN}>Sign in</Link>}
+        {isUserLogged &&
+        <div className="user-block__avatar">
+          <Link to = {Path.MY_LIST}>
+            <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+          </Link>
+        </div>}
       </div>
     </div>);
 };
