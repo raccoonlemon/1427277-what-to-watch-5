d97c4821b121553556d683f17a4d741775a863ae
Irina <raccoonlemon@gmail.com>
2020-11-11 23:03:59 +0600

    Исправлен тип пропса id фильма

--- src/components/films-list/films-list.jsx
+++ src/components/films-list/films-list.jsx
@@ -23,7 +23,7 @@ export const FilmsList = (props) => {
 FilmsList.propTypes = {
   setActiveItem: PropTypes.func.isRequired,
   removeActiveItem: PropTypes.func.isRequired,
-  activeItemId: PropTypes.number,
+  activeItemId: PropTypes.string,
   films: PropTypes.arrayOf(filmShape).isRequired,
 };
 
