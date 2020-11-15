e25550111ef4b8d3d5fa0e43ddaf351b62afd474
Irina <raccoonlemon@gmail.com>
2020-11-15 21:33:05 +0600

    Добавлены тесты для <AddToListButton>

---
+++ src/components/add-to-list-button/add-to-list-button.test.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {AddToListButton} from "./add-to-list-button";
+
+const noop = () => {};
+const id = `1`;
+describe(`<AddToListButton> renders correctly`, () => {
+  it(`film is in the list`, () => {
+    const tree = renderer
+    .create(<AddToListButton
+      id ={id}
+      isFavorite = {true}
+      isUserLogged = {true}
+      onClickAction={noop}
+      redirectAction={noop}/>)
+    .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`film is not in the list`, () => {
+    const tree = renderer
+    .create(<AddToListButton
+      id ={id}
+      isFavorite = {false}
+      isUserLogged = {true}
+      onClickAction={noop}
+      redirectAction={noop}/>)
+    .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+});
