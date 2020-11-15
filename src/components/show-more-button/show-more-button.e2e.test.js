b70e46204cc38b0db63ebbf57df4bcaa6eeae805
Irina <raccoonlemon@gmail.com>
2020-11-15 15:03:39 +0600

    Исправлено форматирование кода

--- src/components/show-more-button/show-more-button.e2e.test.js
+++ src/components/show-more-button/show-more-button.e2e.test.js
@@ -3,16 +3,13 @@ import {configure, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ShowMoreButton from './show-more-button';
 
-
 configure({adapter: new Adapter()});
 
 const onClick = jest.fn();
 
 it(`Click on "Show more" button`, () => {
-  const showMoreButton = shallow(<ShowMoreButton onClick={onClick}/>
-  );
+  const showMoreButton = shallow(<ShowMoreButton onClick={onClick}/>);
   const button = showMoreButton.find(`.catalog__button`);
-
   button.simulate(`click`);
   expect(onClick).toHaveBeenCalledTimes(1);
 });
