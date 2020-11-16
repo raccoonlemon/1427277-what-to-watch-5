a60e200bfaf74997416061756d86794c443e0d3c
Irina <raccoonlemon@gmail.com>
2020-11-16 23:58:57 +0600

    Заменен классовый компонент на функциональный <VideoPlayer>

--- src/components/video-player/video-player.jsx
+++ src/components/video-player/video-player.jsx
@@ -1,48 +1,39 @@
 import PropTypes from 'prop-types';
-import React, {createRef, PureComponent} from "react";
+import React, {createRef, useEffect} from "react";
 import {FILM_PREVIEW_TIMEOUT} from '../../const';
 
-export default class VideoPlayer extends PureComponent {
-
-  constructor(props) {
-    super(props);
-
-    this._videoRef = createRef();
-  }
-
-  componentDidMount() {
-    const video = this._videoRef.current;
-    video.oncanplaythrough = () => {
+export const VideoPlayer = (props) => {
+  const {poster, src} = props;
+  const videoRef = createRef(null);
+  const videoSourceRef = createRef(null);
+  useEffect(()=>{
+    const video = videoRef.current;
+    videoRef.current.oncanplaythrough = () => {
       setTimeout(()=>{
         video.play();
       }, FILM_PREVIEW_TIMEOUT);
     };
-  }
-
-  componentWillUnmount() {
-    const video = this._videoRef.current;
-    video.oncanplaythrough = null;
-  }
-
-  render() {
-    const {poster, src} = this.props;
-    return (
-      <React.Fragment>
-        <video
-          width="280"
-          height="175"
-          muted="muted"
-          poster={poster}
-          ref = {this._videoRef}>
-          <source src={src} ref = {this._videoSourceRef}/>
-        </video>
-      </React.Fragment>);
-  }
-
-}
+    return ()=>{
+      videoRef.current.oncanplaythrough = null;
+    };
+  });
+  return (
+    <React.Fragment>
+      <video
+        width="280"
+        height="175"
+        muted="muted"
+        poster={poster}
+        ref = {videoRef}>
+        <source src={src} ref = {videoSourceRef}/>
+      </video>
+    </React.Fragment>);
+};
 
 VideoPlayer.propTypes = {
   src: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
 };
+
+export default VideoPlayer;
 
