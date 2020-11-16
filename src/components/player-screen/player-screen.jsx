9eb6d02dc6ea8953b0a40f80296d6cbce15a17b5
Irina <raccoonlemon@gmail.com>
2020-11-17 00:14:01 +0600

    Добавлен полноэкранный режим

--- src/components/player-screen/player-screen.jsx
+++ src/components/player-screen/player-screen.jsx
@@ -97,7 +97,9 @@ export const PlayerScreen = (props)=>{
 
           <div className="player__name">{title}</div>
 
-          <button type="button" className="player__full-screen">
+          <button type="button" className="player__full-screen" onClick={()=>{
+            videoRef.current.requestFullscreen();
+          }}>
             <svg viewBox="0 0 27 27" width="27" height="27">
               <use xlinkHref="#full-screen"></use>
             </svg>
