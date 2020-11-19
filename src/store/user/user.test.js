a20e0b413d287992280b946895c97df5e0fcd765
Irina <raccoonlemon@gmail.com>
2020-11-19 22:53:29 +0600

    Правки импортов в тесте для редьюсера

--- src/store/user/user.test.js
+++ src/store/user/user.test.js
@@ -1,11 +1,10 @@
 
 import MockAdapter from "axios-mock-adapter";
-import {ApiURL, Path, RequestStatus} from "../../../const";
-import {createAPI} from "../../../services/api";
-import {logIn} from "../../api-actions";
-import {AuthorizationStatus} from "../../const";
-import {ActionType, loadUser, setAuthorizationStatus} from "../user/user";
-import {user} from "../user";
+import {ApiURL, AuthorizationStatus, Path, RequestStatus} from "../../const";
+import {userAuthInfo} from "../../mocks/user";
+import {createAPI} from "../../services/api";
+import {logIn} from "../api-actions";
+import {ActionType, loadUser, setAuthorizationStatus, userReducer} from "../user/user";
 
 describe(`User action creators work correctly`, () => {
 
@@ -26,11 +25,8 @@ describe(`User action creators work correctly`, () => {
 
 });
 
-
-const api = createAPI(() => {});
-
 it(`Reducer without additional parameters should return initial state`, () => {
-  expect(user(void 0, {})).toEqual({
+  expect(userReducer(void 0, {})).toEqual({
     authorizationStatus: AuthorizationStatus.NO_AUTH,
     user: {},
     requestStatus: RequestStatus.NOT_REQUESTED,
@@ -39,7 +35,7 @@ it(`Reducer without additional parameters should return initial state`, () => {
 });
 
 it(`Reducer should update authorizationStatus to "auth"`, () => {
-  expect(user({
+  expect(userReducer({
     authorizationStatus: AuthorizationStatus.NO_AUTH,
   }, {
     type: ActionType.SET_AUTHORIZATION_STATUS,
@@ -51,24 +47,28 @@ it(`Reducer should update authorizationStatus to "auth"`, () => {
 
 describe(`Async operation work correctly`, () => {
 
+  const api = createAPI(() => {});
+
   // TODO - Доделать тест.
   it(`Should make a correct API call to /login`, () => {
     const apiMock = new MockAdapter(api);
     const dispatch = jest.fn();
-    const fakeUser = user;
-    const test = logIn(fakeUser);
+    const fakeUser = {email: `test@test.ru`, password: `123456`};
+    const loginLoader = logIn(fakeUser);
 
     apiMock
         .onPost(ApiURL.LOGIN)
         .reply(200, [{fake: true}]);
 
-    return test(dispatch, () => {}, api)
+    return loginLoader(dispatch, () => {}, api)
         .then(() => {
           expect(dispatch).toHaveBeenCalledTimes(2);
+
           expect(dispatch).toHaveBeenNthCalledWith(1, {
             type: ActionType.REQUIRED_AUTHORIZATION,
             payload: AuthorizationStatus.AUTH,
           });
+
           expect(dispatch).toHaveBeenNthCalledWith(2, {
             type: ActionType.REDIRECT_TO_ROUTE,
             payload: Path.MAIN_PAGE,
